cb1da6d0afb04640233d3a9780b8ad5cc7b6351f
Zac <thezacattacks@gmail.com>
2019-06-12 23:40:29 -0400

    working on getting shop working

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,6 +1,26 @@
 extends CanvasLayer
 
 onready var count_label = $container/h_box_container/count
+onready var shop_button = $"container/shop and button/shop_button"
+onready var tweener     = $tween
+
+var menu_shown = false
 
 func _process(delta):
 	count_label.set_text(str(tracker.pets))
+	
+	if tracker.pets >= 100 and shop_button.disabled:
+		shop_button.get_material().set_shader_param("enabled", true)
+		shop_button.disabled = false
+
+func _on_shop_button_pressed():
+	if menu_shown:
+		hide_shop()
+	else:
+		show_shop()
+
+func show_shop():
+	pass
+
+func hide_shop():
+	pass
